0a7504594d598d885405b0e7cf3135a485307ba1
Honda99Poulsen <elias.giron@unah.hn>
2022-11-06 21:16:44 -0600

    add code to class App

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import express from 'express';
+import 'dotenv/config'
+import App from './app'
 
-const PORT = process.env.APP_PORT;
+//const PORT = process.env.APP_PORT;
 
-const app = express();
+//const app = express();
 
-app.listen(3000,() => {
-    console.log(`Server running in port: ${PORT}`);
-});
+App.listen(process.env.APP_PORT as unknown as number | 3001);
