a27945a92b954d5b9d1296f1658bdfb34736b6dc
Honda99Poulsen <elias.giron@unah.hn>
2022-11-06 21:44:00 -0600

    Conexion a postgres terminada

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import express from 'express';
 import 'dotenv/config'
 import App from './app'
 
-//const PORT = process.env.APP_PORT;
-
 //const app = express();
 
 App.listen(process.env.APP_PORT as unknown as number | 3001);
